d33a88468378b0bacf4c60a258e6a3b4380eb128
Andrea Hohnsen <a.hohnsen@mail.de>
2024-03-14 16:33:02 +0100

    Add end version of demo

--- pages/api/jokes/[id].js
+++ pages/api/jokes/[id].js
@@ -14,4 +14,18 @@ export default async function handler(request, response) {
 
     response.status(200).json(joke);
   }
+
+  if (request.method === "PUT") {
+    const jokeData = request.body;
+
+    await Joke.findByIdAndUpdate(id, jokeData);
+
+    response.status(200).json({ status: "Joke updated!" });
+  }
+
+  if (request.method === "DELETE") {
+    await Joke.findByIdAndDelete(id);
+
+    response.status(200).json({ message: "success!" });
+  }
 }
