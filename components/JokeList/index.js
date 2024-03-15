012eec8186de87c5566a9e8096251eccd324b6b7
Andrea Hohnsen <a.hohnsen@mail.de>
2024-03-15 12:50:19 +0100

    Add functionality to delete jokes on homepage

--- components/JokeList/index.js
+++ components/JokeList/index.js
@@ -2,7 +2,7 @@ import useSWR from "swr";
 import Link from "next/link";
 
 export default function JokeList() {
-  const { data, isLoading } = useSWR("/api/jokes");
+  const { data, isLoading, mutate } = useSWR("/api/jokes");
 
   if (isLoading) {
     return <h1>Loading...</h1>;
@@ -12,11 +12,26 @@ export default function JokeList() {
     return;
   }
 
+  async function handleDelete(id) {
+    const response = await fetch(`/api/jokes/${id}`, {
+      method: "DELETE",
+    });
+
+    if (response.ok) {
+      mutate();
+    }
+  }
+
   return (
     <ul>
       {data.map((joke) => (
         <li key={joke._id}>
           <Link href={`/${joke._id}`}>{joke.joke}</Link>
+          <button onClick={() => handleDelete(joke._id)}>
+            <span role="img" aria-label="A cross indicating deletion">
+              ❌
+            </span>
+          </button>
         </li>
       ))}
     </ul>
